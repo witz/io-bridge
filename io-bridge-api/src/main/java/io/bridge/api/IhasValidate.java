ff2e30553a4a8d4e8bcb584fe96a5e2baf4f892e
Witz <witz@users.noreply.github.com>
2020-03-31 20:28:30 +0200

    Updated javadoc

--- io-bridge-api/src/main/java/io/bridge/api/IhasValidate.java
+++ io-bridge-api/src/main/java/io/bridge/api/IhasValidate.java
@@ -12,10 +12,10 @@ package io.bridge.api;
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -29,7 +29,8 @@ package io.bridge.api;
 import io.bridge.api.exceptions.InvalidAttributeException;
 
 /**
- * *Internal* Interface to designate type having a {@link #validate()} method.
+ * *Internal* Interface to designate types supporting validation through a
+ * dedicated {@link #validate()} method.
  *
  * @author Charaf-Eddine SAIDI
  *
