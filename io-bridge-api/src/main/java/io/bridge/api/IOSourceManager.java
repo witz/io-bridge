255339c685685cc234803605afd7b630480e6511
Witz <witz@users.noreply.github.com>
2020-03-31 21:52:58 +0200

    converted source manager to final class

--- io-bridge-api/src/main/java/io/bridge/api/IOSourceManager.java
+++ io-bridge-api/src/main/java/io/bridge/api/IOSourceManager.java
@@ -12,10 +12,10 @@ package io.bridge.api;
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -38,27 +38,26 @@ import org.apache.commons.lang3.StringUtils;
  * @author Charaf-Eddine SAIDI
  *
  */
-public enum IOSourceManager {
-    INSTANCE;
+public final class IOSourceManager {
 
-    private final ServiceLoader<IOSource> loader;
+    private static final ServiceLoader<IOSource> LOADER = ServiceLoader.load(IOSource.class);
 
     private IOSourceManager() {
-        loader = ServiceLoader.load(IOSource.class);
+        // private constructor to hide default one
     }
 
-    public List<IOSource> getIOSources(boolean refresh) {
+    public static List<IOSource> getIOSources(boolean refresh) {
         if (refresh) {
-            loader.reload();
+            LOADER.reload();
         }
-        return IteratorUtils.toList(loader.iterator());
+        return IteratorUtils.toList(LOADER.iterator());
     }
 
-    public Optional<IOSource> getIOSource(String name) {
+    public static Optional<IOSource> getIOSource(String name) {
         return getIOSource(name, false);
     }
 
-    public Optional<IOSource> getIOSource(String name, boolean refresh) {
+    public static Optional<IOSource> getIOSource(String name, boolean refresh) {
         List<IOSource> sources = getIOSources(refresh);
         return sources.stream().filter(s -> StringUtils.equals(name, s.getName())).findFirst();
     }
